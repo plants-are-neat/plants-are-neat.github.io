f10e0c4ae4bd746c98582a026cf78fc80b15d63c
Noah Spahn <nspahn@bu.edu>
2020-11-19 00:37:14 -0500

    syntax error fix
    syntax error fix

--- upload_dummy.m
+++ upload_dummy.m
@@ -2,4 +2,4 @@
 
 
 tStamp = datetime('now')
-thingSpeakWrite(1226802,[20.3,10.2,30.2,0.1],'WriteKey'›‹,'9M50MDHK1RPV9KW8','TimeStamp',tStamp)
+thingSpeakWrite(1226802,[90.3,10.2,30.2,0.1],'WriteKey','9M50MDHK1RPV9KW8','TimeStamp',tStamp)
