17a44dd69db76d558e5d965910fa06ada621f131
SergioAPareja <45397958+SergioAPareja@users.noreply.github.com>
2021-01-06 20:00:43 -0400

    Create NodeMeshV.02.ino

---
+++ Radio/NodeMeshV.02.ino
@@ -0,0 +1,136 @@
+
+#include <RHRouter.h>
+#include <RHMesh.h>
+#include <RH_RF95.h>
+#define RH_HAVE_SERIAL
+#define LED 9
+#define N_NODES 4
+
+uint8_t nodeId;
+uint8_t routes[N_NODES]; // full routing table for mesh
+int16_t rssi[N_NODES]; // signal strength info
+
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHMesh *manager;
+
+// message buffer
+char buf[RH_MESH_MAX_MESSAGE_LEN];
+
+
+void setup() {
+  randomSeed(analogRead(0));
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) ; // Wait for serial port to be available
+
+  Serial.print(F("initializing node "));
+
+  manager = new RHMesh(rf95, nodeId);
+
+  if (!manager->init()) {
+    Serial.println(F("init failed"));
+  } else {
+    Serial.println("done");
+  }
+  rf95.setTxPower(23, false);
+  rf95.setFrequency(915.0);
+  rf95.setCADTimeout(500);
+
+  // Possible configurations:
+  // Bw125Cr45Sf128 (the chip default)
+  // Bw500Cr45Sf128
+  // Bw31_25Cr48Sf512
+  // Bw125Cr48Sf4096
+
+  // long range configuration requires for on-air time
+  boolean longRange = true;
+  if (longRange) {
+    RH_RF95::ModemConfig modem_config = {
+      0x78, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit
+      0xC4, // Reg 0x1E: Spread=4096chips/symbol, CRC=enable
+      0x08  // Reg 0x26: LowDataRate=On, Agc=Off.  0x0C is LowDataRate=ON, ACG=ON
+    };
+    rf95.setModemRegisters(&modem_config);
+    if (!rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096)) {
+      Serial.println(F("set config failed"));
+    }
+  }
+
+  Serial.println("RF95 ready");
+
+  for(uint8_t n=1;n<=N_NODES;n++) {
+    routes[n-1] = 0;
+    rssi[n-1] = 0;
+  }
+
+  Serial.print(F("mem = "));
+
+}
+
+const __FlashStringHelper* getErrorString(uint8_t error) {
+  switch(error) {
+    case 1: return F("invalid length");
+    break;
+    case 2: return F("no route");
+    break;
+    case 3: return F("timeout");
+    break;
+    case 4: return F("no reply");
+    break;
+    case 5: return F("unable to deliver");
+    break;
+  }
+  return F("unknown");
+}
+
+void updateRoutingTable() {
+  for(uint8_t n=1;n<=N_NODES;n++) {
+    RHRouter::RoutingTableEntry *route = manager->getRouteTo(n);
+    if (n == nodeId) {
+      routes[n-1] = 255; // self
+    } else {
+      routes[n-1] = route->next_hop;
+      if (routes[n-1] == 0) {
+        // if we have no route to the node, reset the received signal strength
+        rssi[n-1] = 0;
+      }
+    }
+  }
+}
+
+// Create a JSON string with the routing info to each node
+void getRouteInfoString(char *p, size_t len) {
+  p[0] = '\0';
+  strcat(p, "[");
+  for(uint8_t n=1;n<=N_NODES;n++) {
+    strcat(p, "{\"n\":");
+    sprintf(p+strlen(p), "%d", routes[n-1]);
+    strcat(p, ",");
+    strcat(p, "\"r\":");
+    sprintf(p+strlen(p), "%d", rssi[n-1]);
+    strcat(p, "}");
+    if (n<N_NODES) {
+      strcat(p, ",");
+    }
+  }
+  strcat(p, "]");
+}
+
+void printNodeInfo(uint8_t node, char *s) {
+  Serial.print(F("node: "));
+  Serial.print(F("{"));
+  Serial.print(F("\""));
+  Serial.print(node);
+  Serial.print(F("\""));
+  Serial.print(F(": "));
+  Serial.print(s);
+  Serial.println(F("}"));
+}
+
+void loop() {
+  
+  }
