d06645520882e9e9e67acb5df969a44e43423ad3
emperez <35668165+emperez@users.noreply.github.com>
2021-01-28 21:00:36 -0500

    Update sensor_node_1.ino
    Same as the previous commit

--- sensor_node_1/sensor_node_1.ino
+++ sensor_node_1/sensor_node_1.ino
@@ -6,26 +6,45 @@
 
 #include <RHMesh.h>
 #include <RH_RF95.h>
+#include <RHGenericDriver.h>
+#include <RHRouter.h>
 #include <SPI.h>
 
 #define CLIENT_ADDRESS 1
 #define SERVER1_ADDRESS 2
+#define THERM A0
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
 
 //radio driver
-RH_RF95 driver;
+RH_RF95 driver(RFM95_CS, RFM95_INT);
 
-//set up manager
-RHMesh manager(driver, SERVER1_ADDRESS);
-
+// set up manager
+RHMesh manager(driver, CLIENT_ADDRESS);
 
 void setup() 
 {
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
   Serial.begin(9600);
+  while(!Serial)
   if (!manager.init())
-    Serial.println("RF95 init failed");
-
-  driver.setTxPower(23, false);
-  driver.setFrequency(RF95_FREQ);
+    Serial.println("init failed");
+  while (!driver.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+     
+ // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!driver.setFrequency(RF95_FREQ)) {
+   Serial.println("setFrequency failed");
+   while (1);
+   }
+   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+   driver.setTxPower(23, false);
 }
 
 
