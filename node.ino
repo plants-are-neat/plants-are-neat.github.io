d72b68543c9ee3e442bb8bb0d1cf526d2a528306
blivney <35508079+blivney@users.noreply.github.com>
2021-02-09 16:26:49 -0500

    Update VH400 calculations, set data to 1 decimal place

--- node.ino
+++ node.ino
@@ -16,8 +16,8 @@ void setup() {
 	
 	// Setup VEML7700
 	veml.begin();
-	veml.setGain(VEML7700_GAIN_1);
-	veml.setIntegrationTime(VEML7700_IT_800MS);
+	veml.setGain(VEML7700_GAIN_1_8); // largest light range
+	veml.setIntegrationTime(VEML7700_IT_100MS);
 	
 	// Setup SHT31-D
 	sht.begin(0x44);
@@ -35,26 +35,32 @@ void loop() {
 
 String collectData() {
 	// Get soil temperature with thermistor
-	float resistance = analogRead(THERM);
+	float resistance = analogRead(THERM); // get 0-1023 value from thermistor
 	resistance = (1023 / resistance) - 1;
-	resistance = 10000.0 / resistance;
+	resistance = 10000.0 / resistance; // get thermistor actual resistance
 	float B = 3935; // depends on the NTC model
 	float soiltemp = log(resistance / 10000) / B;
 	soiltemp += 1.0 / (25 + 273.15);
 	soiltemp = 1.0 / soiltemp;
-	soiltemp -= 273.15;
+	soiltemp -= 273.15; // apply thermistor characteristic B parameter equation
 	soiltemp = soiltemp * (9.0 / 5.0) + 32; // units of degrees F
+	soiltemp = round(soiltemp * 10) / 10; // one decimal place
 	
 	// Get soil moisture with VH400
-	float moisture = analogRead(VH400);
-	moisture = map(moisture, 0, 1023, 0, 100); // units of %
+	float moisture = analogRead(VH400); // get 0-1023 value from VH400
+	float maxMoistureVoltage = (3 * 1023) / 3.3; // VH400 output voltage is 0V - 3V
+	moisture = map(moisture, 0, maxMoistureVoltage, 0, 100); // units of %
+	moisture = round(soiltemp * 10) / 10; //one decimal place
 	
 	// Get lux from VEML7700
 	float lux = veml.readLux(); // units of Lux
+	lux = round(lux * 10) / 10; // one decimal place
 
 	// Get temp and humidity from SHT31-D
 	float temp = sht.readTemperature() * (9.0 / 5.0) + 32; // units of degrees F
+	temp = round(temp * 10) / 10; // one decimal place
 	float humidity = sht.readHumidity(); // units of %
+	humidity = round(humidito * 10) / 10; // one decimal place
 	
 	// Return data
 	String results = String(temp) + "," + String(humidity) + "," + String(lux) + "," + String(moisture) + "," + String(soiltemp);
