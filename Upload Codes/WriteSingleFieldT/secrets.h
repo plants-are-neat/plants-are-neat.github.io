2ecf91b2b3ef0d75357d400c2f50e7136c7b8eab
Noah S <72777308+nspahn@users.noreply.github.com>
2021-03-02 23:37:03 -0500

    Add files via upload

---
+++ Upload Codes/WriteSingleFieldT/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "BigBlind"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "Fantastic"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1310790			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "US1U1HBGB67KI4FK"   // replace XYZ with your channel write API Key
