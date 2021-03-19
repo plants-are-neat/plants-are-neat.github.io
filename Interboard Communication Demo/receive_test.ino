5079d52a5c6d937983f3b0c7b89eb21926e69ae9
blivney <35508079+blivney@users.noreply.github.com>
2021-03-19 09:54:13 -0400

    Implement communication protocol

---
+++ Interboard Communication Demo/receive_test.ino
@@ -0,0 +1,54 @@
+// Data pins D5 D6 D7 D8
+#define latchIN D2
+#define recOUT D1
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(D5, INPUT);
+  pinMode(D6, INPUT);
+  pinMode(D7, INPUT);
+  pinMode(D8, INPUT);
+  pinMode(latchIN, INPUT);
+  pinMode(recOUT, OUTPUT);
+  digitalWrite(recOUT, LOW);
+  delay(1000);
+}
+
+void loop() {
+  String msg = "huh ";
+  if(checkLatch()) {
+    String s = getData();
+    if (s[0] != '|') {
+      msg.concat(s);
+      Serial.print(s);
+      Serial.println(msg);
+    } else {
+      // Push message to console over Wi-Fi
+      Serial.println(msg);
+      Serial.println("complete");
+    }
+    resetData();
+    delay(10);
+  }
+}
+
+bool checkLatch() {
+  return ((digitalRead(latchIN) == HIGH) ? true : false);
+}
+
+String getData() {
+  int num = 0;
+  num += ((digitalRead(D5) == HIGH) ? 1 : 0) +
+    ((digitalRead(D6) == HIGH) ? 2 : 0) +
+    ((digitalRead(D7) == HIGH) ? 4 : 0) +
+    ((digitalRead(D8) == HIGH) ? 8 : 0);
+  if (num == 14) return String(",");
+  if (num == 15) return String("|");
+  return String(num);
+}
+
+void resetData() {
+  digitalWrite(recOUT, HIGH);
+  delay(10);
+  digitalWrite(recOUT, LOW);
+}
---
+++ Interboard Communication Demo/send_test.ino
@@ -0,0 +1,60 @@
+int datapins[] = {10, 11, 12, 13};
+#define latch 9
+#define rec 6
+void setup() {
+  Serial.begin(9600);
+  for (int i = 10; i <= 13; i++) {
+    pinMode(i, OUTPUT);
+  }
+  pinMode(latch, OUTPUT);
+  pinMode(rec, INPUT);
+  digitalWrite(latch, LOW);
+  Serial.println("initialized");
+}
+
+void loop() {
+  String msg = "123456789,10|";
+  int num = 0;
+  for (int i = 0; i < msg.length(); i++) {
+    //Serial.println(num);
+    if (msg[i] == ',') {
+      num = 14;
+    } else if (msg[i] == '|') {
+      num = 15;
+    } else {
+      num = 6; //(int) msg[i];
+    }
+    Serial.println(num);
+    sendBin(num);
+    pulseLatch();
+    resetData();
+  }
+  delay(5000);
+}
+
+void sendBin(int num) {
+  String s = String(num, BIN);
+  while (s.length() < 4) {
+    s = String(0) + s;
+  }
+  for (int i = 0; i < 4; i++) {
+    if (s[i] == '0') {
+      digitalWrite(datapins[i], LOW);
+    } else {
+      digitalWrite(datapins[i], HIGH);
+    }
+  }
+  delay(1);
+}
+
+void pulseLatch() {
+  digitalWrite(latch, HIGH);
+  delay(10);
+  digitalWrite(latch, LOW);
+}
+
+void resetData() {
+  while (digitalRead(rec) != HIGH) {}
+  sendBin(15);
+  delay(10);
+}
